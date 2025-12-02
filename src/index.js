3ba3a10bfe1b5879f84e13c002ff1fa926c8492a
Samuel <samawyck@gmail.com>
2025-12-02 12:06:30 -0600

    Test current turing machine class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("hi")
+const TuringMachine = require("./turing-machine.js");
+
+
+
+const antsInfo = [
+    [{"W": "left", "B": "right", "R": "left", "G": "right"}, {"G": "W", "B": "W", "R": "B", "W": "B"}]
+];
+
+const machine = new TuringMachine(antsInfo, 10, 10);
+
+for (let i = 0; i < 2; i += 1) {
+    machine.update()
+    for (let ant of machine.ants) {
+        console.log(ant.row, ant.col);
+    }
+    console.log(machine.board);
+}
