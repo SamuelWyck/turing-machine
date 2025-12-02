36b826a6c596a12f146d3cf1128194d7a7e49d9f
Samuel <samawyck@gmail.com>
2025-12-02 12:05:49 -0600

    Create turing machine class

---
+++ src/turing-machine.js
@@ -0,0 +1,62 @@
+const Ant = require("./ant.js");
+
+
+
+class TuringMachine {
+    constructor(antsInfo, width, height) {
+        this.width = width;
+        this.height = height;
+        this.whiteSymbol = "W";
+        this.board = this.#generateBoard();
+        this.createAnts(antsInfo);
+    };
+
+    #generateBoard() {
+        const board = [];
+        for (let i = 0; i < this.height; i += 1) {
+            const row = [];
+            for (let j = 0; j < this.width; j += 1) {
+                row.push(this.whiteSymbol);
+            }
+            board.push(row);
+        }
+        return board;
+    };
+
+    createAnts(antsInfo) {
+        this.antsInfo = antsInfo;
+        this.ants = [];
+        for (let antInfo of antsInfo) {
+            const [antStates, colorSwapMap] = antInfo;
+            const [row, col] = this.#getRandomCoords();
+            const ant = new Ant(row, col, antStates, colorSwapMap, this.width, this.height);
+            this.ants.push(ant);
+        }
+    };
+
+    #getRandomCoords() {
+        const row = this.#randInt(0, this.height);
+        const col = this.#randInt(0, this.width);
+        return [row, col];
+    };
+
+    #randInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min);
+    };
+
+    update() {
+        for (let ant of this.ants) {
+            const oldRow = ant.row;
+            const oldCol = ant.col;
+            const color = this.board[ant.row][ant.col];
+            const newColor = ant.move(color);
+            this.board[oldRow][oldCol] = newColor;
+        };
+    };
+};
+
+
+
+module.exports = TuringMachine;
