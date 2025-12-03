a78fc99e46e6658f320e608f8a8f9e9a95388267
Samuel <samawyck@gmail.com>
2025-12-03 12:47:52 -0600

    Test display

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
+require("./styles/index.css");
 const TuringMachine = require("./turingMachine/turingMachine.js");
+const BoardDisplay = require("./boardDisplay.js");
 
 
 
 const antsInfo = [
-    [{"W": "left", "B": "right", "R": "left", "G": "right"}, {"G": "W", "B": "W", "R": "B", "W": "B"}]
+    [{"W": "left", "B": "right", "R": "straight", "G": "turn around"}, {"G": "R", "B": "W", "R": "G", "W": "B"}]
 ];
 
-const machine = new TuringMachine(antsInfo, 10, 10);
 
-for (let i = 0; i < 2; i += 1) {
-    machine.update()
-    for (let ant of machine.ants) {
-        console.log(ant.row, ant.col);
-    }
-    console.log(machine.board);
+// for (let i = 0; i < 2; i += 1) {
+//     machine.update()
+//     for (let ant of machine.ants) {
+//         console.log(ant.row, ant.col);
+//     }
+//     console.log(machine.board);
+// }
+        
+        
+const machine = new TuringMachine(antsInfo, 100, 100);
+const display = new BoardDisplay(100, 100);
+display.setAntPositions(machine.ants);
+
+function test() {
+    const updatedAnts = machine.update();
+    display.updateAnts(updatedAnts);
+    setTimeout(test, 100);
 }
+test()
