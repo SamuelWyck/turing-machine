43406ab4fafd5144ab743929421a05910d47872f
Samuel <samawyck@gmail.com>
2025-12-05 13:05:12 -0600

    Add default class to board div on creation

--- src/boardDisplay.js
+++ src/boardDisplay.js
@@ -10,7 +10,7 @@ class BoardDisplay {
         this.straightSymbol = "G";
         this.turnAroundSymbol = "R";
 
-        this.board = createElement("div", null, "board");
+        this.board = createElement("div", null, "board", "default");
         const boardContainer = document.querySelector("div.board-wrapper");
         boardContainer.appendChild(this.board);
 
