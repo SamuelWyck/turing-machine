9a5b296a1596b5dde121983d225abc61cbd9098d
Samuel <samawyck@gmail.com>
2025-12-05 14:03:48 -0600

    Create turingMachineInterface class and add logic to change machine speed

---
+++ src/turingMachineInterface.js
@@ -0,0 +1,76 @@
+const TuringMachine = require("./turingMachine/turingMachine.js");
+const BoardDisplay = require("./boardDisplay.js");
+
+
+class TuringMachineInterFace {
+    constructor() {
+        const antsInfo = [
+            TuringMachine.fourStateAnt1, 
+            TuringMachine.fourStateAnt2, 
+            TuringMachine.twoStateAnt1,
+            TuringMachine.twoStateAnt2
+        ];
+        const boardSize = 100;
+        this.turingMachine = new TuringMachine(antsInfo, boardSize, boardSize);
+        this.display = new BoardDisplay(boardSize, boardSize);
+        this.display.setAntPositions(this.turingMachine.ants);
+
+        this.run = true;
+        this.speeds = [500, 250, 100, 0, 1];
+        this.speedBtnClassMap = {500: "one", 250: "two", 100: "five", 0: "max", 1: "stop"};
+        this.speedIndex = 0;
+
+        this.machineLoop = this.machineLoop.bind(this);
+        this.optionsBtnCallback = this.optionsBtnCallback.bind(this);
+        this.initializeButtons();
+    };
+
+    machineLoop() {
+        const updatedAnts = this.turingMachine.update();
+        this.display.updateAnts(updatedAnts);
+
+        if (this.run) {
+            setTimeout(this.machineLoop, this.speeds[this.speedIndex]);
+        }
+    };
+
+    start() {
+        this.run = true;
+        this.machineLoop();
+    };
+
+    stop() {
+        this.run = false;
+    };
+
+    optionsBtnCallback(event) {
+        const btn = (event.target.matches("button")) ? event.target : event.target.parentElement;
+        if (btn.matches(".speed")) {
+
+            btn.classList.remove("one", "two", "five", "max", "stop");
+            this.speedIndex = (this.speedIndex + 1 === this.speeds.length) ? 0 : this.speedIndex + 1;
+            const speed = this.speeds[this.speedIndex];
+            if (speed === 1) {
+                this.stop();
+            } else if (!this.run) {
+                this.start();
+            }
+            const btnClass = this.speedBtnClassMap[speed];
+            btn.classList.add(btnClass);
+
+        } else if (btn.matches(".palette")) {
+
+        } else if (btn.matches(".settings")) {
+
+        }
+    };
+
+    initializeButtons() {
+        const optionsDiv = document.querySelector("div.options");
+        optionsDiv.addEventListener("click", this.optionsBtnCallback);
+    };
+};
+
+
+
+module.exports = TuringMachineInterFace;
