594724c3889e2faa5c32cd5ccc3dd2394e6e1316
Samuel <samawyck@gmail.com>
2025-12-05 15:08:55 -0600

    Add palette six

--- src/turingMachineInterface.js
+++ src/turingMachineInterface.js
@@ -29,7 +29,14 @@ class TuringMachineInterFace {
         this.speedIndex = 0;
 
         this.paletteIndex = 0;
-        this.paletteClasses = ["paletteOne", "paletteTwo", "paletteThree", "paletteFour", "paletteFive"];
+        this.paletteClasses = [
+            "paletteOne", 
+            "paletteTwo", 
+            "paletteThree", 
+            "paletteFour", 
+            "paletteFive", 
+            "paletteSix"
+        ];
 
         this.machineLoop = this.machineLoop.bind(this);
         this.optionsBtnCallback = this.optionsBtnCallback.bind(this);
