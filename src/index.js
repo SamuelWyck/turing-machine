4330593d0c2aefaea3f77b3ff674b4236b0c1ee0
Samuel <samawyck@gmail.com>
2025-12-05 14:04:07 -0600

    Test turingMachineInterface

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
 require("./styles/index.css");
-const TuringMachine = require("./turingMachine/turingMachine.js");
-const BoardDisplay = require("./boardDisplay.js");
+const TuringMachineInterFace = require("./turingMachineInterface.js");
+// const TuringMachine = require("./turingMachine/turingMachine.js");
+// const BoardDisplay = require("./boardDisplay.js");
 
 
-const antsInfo = [
-    TuringMachine.fourStateAnt1, 
-    TuringMachine.fourStateAnt2, 
-    TuringMachine.twoStateAnt1,
-    TuringMachine.twoStateAnt2
-];
+// const antsInfo = [
+//     TuringMachine.fourStateAnt1, 
+//     TuringMachine.fourStateAnt2, 
+//     TuringMachine.twoStateAnt1,
+//     TuringMachine.twoStateAnt2
+// ];
 
         
         
-const machine = new TuringMachine(antsInfo, 100, 100);
-const display = new BoardDisplay(100, 100);
-display.setAntPositions(machine.ants);
+// const machine = new TuringMachine(antsInfo, 100, 100);
+// const display = new BoardDisplay(100, 100);
+// display.setAntPositions(machine.ants);
 
-function test() {
-    const updatedAnts = machine.update();
-    display.updateAnts(updatedAnts);
-    setTimeout(test, 0);
-};
-test();
+// function test() {
+//     const updatedAnts = machine.update();
+//     display.updateAnts(updatedAnts);
+//     setTimeout(test, 0);
+// };
+// test();
+
+const interface = new TuringMachineInterFace();
+interface.start();
